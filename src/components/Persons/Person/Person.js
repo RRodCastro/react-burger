e51c9cf7bfe9158b3ecfd18576bee0181d37c648
Rodrigo <mrcastro9574@gmail.com>
2020-05-23 18:16:42 -0600

    Side effect in function component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
 import classes from './Person.css'
 import wrapper from '../../Hoc/wrapper'
 import PropTypes from 'prop-types'
@@ -6,10 +6,19 @@ import PropTypes from 'prop-types'
 
 const person = (props) => {
 
+    const nameInputRef = useRef(null)
+
+    useEffect( () =>  {
+        nameInputRef.current.value = nameInputRef.current.value + ' &&'
+    
+        // Trigger just after first render
+    }, []) ;
+
     return (
         <Fragment>
             <div onClick={props.deletePersonHandler} >My name is {props.name} and {props.age} years old. </div>
             <input
+                ref={nameInputRef}
                 stlye={{ display: 'block' }}
                 onChange={props.changed}
                 value={props.name}
