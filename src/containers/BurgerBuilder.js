bd35f695d999da3d41456c704dbcb3806d55be16
Rodrigo <mrcastro9574@gmail.com>
2020-05-24 10:59:42 -0600

    Add hoc wrapper component

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Aux from '../hoc/Aux'
+import Aux from '../hoc/Wrapper'
 import Burger from '../components/Burger/Burger';
 
 class BurguerBuilder extends Component {
